42dedc67eaaa2e3a2852576774b7abb04772de88
Thasnima Begum <thasnima@hotmail.co.uk>
2023-11-02 15:58:41 +0000

    growUp method added

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -10,4 +10,22 @@ describe("constructor", ()=>{
         const pet = new Pet("Fido")
         expect(pet.name).toEqual("Fido")
     })
+
+    test("returns initial age as 0", ()=>{
+        const pet = new Pet("Fido", 0)
+        expect(pet.age).toEqual(0)
+    })
+
+    test("increments age by 1", ()=>{
+        const pet = new Pet("Fido", 0)
+        pet.growUp()
+        expect(pet.age).toEqual(1)
+    })
+
+    test("increments age by 2", ()=>{
+        const pet = new Pet("Fido", 0)
+        pet.growUp()
+        pet.growUp()
+        expect(pet.age).toEqual(2)
+    })
 })

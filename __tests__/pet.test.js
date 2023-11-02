ed42df66f6e3a9d7f31216e6f2b3b501e2655abd
Thasnima Begum <thasnima@hotmail.co.uk>
2023-11-02 21:53:15 +0000

    walk function added

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -12,32 +12,55 @@ describe("constructor", ()=>{
     })
 
     test("returns initial age as 0", ()=>{
-        const pet = new Pet("Fido", 0)
+        const pet = new Pet("Fido")
         expect(pet.age).toEqual(0)
     })
+})
+
+describe("growUp", ()=>{
 
     test("increments age by 1", ()=>{
-        const pet = new Pet("Fido", 0)
+        const pet = new Pet("Fido")
         pet.growUp()
         expect(pet.age).toEqual(1)
     })
 
     test("increments age by 2", ()=>{
-        const pet = new Pet("Fido", 0)
+        const pet = new Pet("Fido")
         pet.growUp()
         pet.growUp()
         expect(pet.age).toEqual(2)
     })
 
     test("growUp function increments hunger by 5", ()=>{
-        const pet = new Pet("Fido", 0)
+        const pet = new Pet("Fido")
         pet.growUp()
         expect(pet.hunger).toEqual(5)
     })
 
     test("growUp function decrements fitness by 3", ()=>{
-        const pet = new Pet("Fido", 0)
+        const pet = new Pet("Fido")
         pet.growUp()
         expect(pet.fitness).toEqual(7)
     })
+
 })
+
+describe("walk", ()=> {
+
+    test("walk", ()=>{
+        const pet = new Pet("Fido")
+        pet.fitness = 4
+        pet.walk()
+        expect(pet.fitness).toEqual(8)
+    })
+
+    test("walk", ()=>{
+        const pet = new Pet("Fido")
+        pet.fitness = 7
+        pet.walk()
+        expect(pet.fitness).toEqual(10)
+    })
+})
+
+
--- src/pet.js
+++ src/pet.js
@@ -1,17 +1,29 @@
+const MAXIMUM_FITNESS = 10;
+const INITIAL_AGE = 0
+
 function Pet (name) {
     this.name = name;
-    this.age = 0;
+    this.age = INITIAL_AGE;
     this.hunger = 0;
-    this.fitness = 10
+    this.fitness = MAXIMUM_FITNESS
 }
 
-Pet.prototype = {
-    growUp () {
+Pet.prototype.growUp = function() {
         this.age++;
         this.hunger+=5;
         this.fitness-=3;
     }
-}
+
+    Pet.prototype.walk = function() {
+        if (this.fitness+4<=MAXIMUM_FITNESS) {
+            this.fitness+=4
+        }
+        else {
+            this.fitness = MAXIMUM_FITNESS
+        }
+    }
+
+
 
 module.exports = Pet
 
